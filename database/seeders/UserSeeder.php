673e7ce40feea3fde32661efaaf694e0b0a120a9
Alfonsus Vega <130917320+VgTan@users.noreply.github.com>
2025-05-28 18:19:46 +0700

    userSeederUpdate

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -22,11 +22,71 @@ class UserSeeder extends Seeder
             ],
         );
         User::updateOrCreate(
-            ['email' => 'rama@sinerco.com'],
+            [
+                'email' => 'ramadhans.businessmail@gmail.com'
+            ],
             [
                 'name' => 'Rama',
                 'password' => Hash::make('admin12345'),
-                'role' => 'admin',
+                'role' => 'super_admin',
+                'whatsAppNum' => '081359113349'
+            ],
+        );
+        User::updateOrCreate(
+            ['email' => 'johan@sinerco.co.id'],
+            [
+                'name' => 'Johan',
+                'password' => Hash::make('admin12345'),
+                'role' => 'super_admin',
+                'whatsAppNum' => '082113837546'
+            ],
+        );
+        User::updateOrCreate(
+            ['email' => 'operator@sinerco.co.id'],
+            [
+                'name' => 'Operator',
+                'password' => Hash::make('operator12345'),
+                'role' => 'operator',
+            ],
+        );
+        User::updateOrCreate(
+            ['email' => 'teknisi@sinerco.co.id'],
+            [
+                'name' => 'Teknisi',
+                'password' => Hash::make('teknisi12345'),
+                'role' => 'technition',
+            ],
+        );
+        User::updateOrCreate(
+            ['email' => 'client@sinerco.co.id'],
+            [
+                'name' => 'Client',
+                'password' => Hash::make('client12345'),
+                'role' => 'client',
+            ],
+        );
+        User::updateOrCreate(
+            ['email' => 'manager@sinerco.co.id'],
+            [
+                'name' => 'Manager',
+                'password' => Hash::make('manager12345'),
+                'role' => 'guest',
+            ],
+        );
+         User::updateOrCreate(
+            ['email' => 'general.manager@sinerco.co.id'],
+            [
+                'name' => 'General Manager',
+                'password' => Hash::make('general.manager12345'),
+                'role' => 'guest',
+            ],
+        );
+        User::updateOrCreate(
+            ['email' => 'direksi@sinerco.co.id'],
+            [
+                'name' => 'direksi',
+                'password' => Hash::make('direksi12345'),
+                'role' => 'guest',
             ],
         );
     }
